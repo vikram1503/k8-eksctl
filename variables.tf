561bedb736dd4ccd93c6ea6f47fa6bca2d62283b
vikram1503 <vikram15031989@gmail.com>
2024-07-09 16:51:50 +0530

    practice

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-07832342d5cdd6bb4"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-047891b2b720a7614"
+}
